c7a78de4fd36eb89b8933ddde7a53fc6b9c1f281
DigiThax <digithax@gmail.com>
2024-01-20 12:17:30 +0530

    Updated Main, added functions to ingest and generate basic markdown

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-import os
-import glob
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as xml
 
-from pprint import pprint
+class Pycoco:
 
-# First implementation will have the script along side the report.
-# After this matures in feature, parameter inputs for path will be made available
-base_directory = os.getcwd() + '\\html'
+    def __init__(self, file_path):
+        # Empty for now
+        self.file_path = file_path
 
-# Find all index.html files
-index_files = (glob.glob(os.path.join(base_directory, '**/index.html'), recursive=True))
+    def import_report(self):
+        report = xml.parse(self.file_path)
+        report = report.getroot()
+
+        # Extract Relevant Data
+        data = []
+        for package in report.findall(".//package"):
+            package_name = package.get('name')
+            for class_element in package.findall('class'):
+                class_name = class_element.get('name')
+                line_coverage = class_element.find('.//counter[@type="LINE"]').get('covered')
+                data.append({'Package': package_name, 'Class': class_name, 'Line Coverage': line_coverage})
+        
+        return data
+    
+    def generate_report(self, data, output_file):
+        with open(output_file, 'w') as md_file:
+            md_file.write('# JaCoCo Coverage Report\n\n')
+            for item in data:
+                md_file.write(f"## Package: {item['Package']}\n")
+                md_file.write(f"### Class: {item['Class']}\n")
+                md_file.write(f"- Line Coverage: {item['Line Coverage']}%\n\n")
 
 
-for index_file in index_files:
-    with open(index_file, 'r') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        pprint(soup)
-        exit()
+if __name__ == "__main__":
+    pycoco = Pycoco('sample.xml')
+    markdown_output_path = 'jacoco_report.md'
+
+    data = pycoco.import_report()
+    pycoco.generate_report(data, markdown_output_path)
+    print(f"Markdown report generated at {markdown_output_path}")
